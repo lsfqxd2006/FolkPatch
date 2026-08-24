47fd36c946be9012cdca7006f3f7da8a71546ba5
lsfqxd2006 <liusongfeng1030@163.com>
2026-08-24 16:46:50 +0800

    Update SwipeNavigationHandler.kt

--- app/src/main/java/me/bmax/apatch/ui/navigation/SwipeNavigationHandler.kt
+++ app/src/main/java/me/bmax/apatch/ui/navigation/SwipeNavigationHandler.kt
@@ -67,7 +67,7 @@ fun Modifier.swipeToSwitchTab(
     onSwipeComplete: () -> Unit
 ): Modifier {
     val destinations = rememberVisibleDestinations()
-    val thresholdPx = with(LocalDensity.current) { 35.dp.toPx() }
+    val thresholdPx = with(LocalDensity.current) { 30.dp.toPx() }
 
     return this.pointerInput(destinations, navController, onSwipeStart, onSwipeComplete, thresholdPx) {
         var totalX = 0f
